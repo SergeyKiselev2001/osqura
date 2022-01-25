ccb9798750d0e7a8fbe33795ac1f2a1ef5fa28df
Kiselev Sergey <kissergey98@mail.ru>
2022-01-25 14:00:16 +0300

    refactoring 2

--- src/Components/Forms.jsx
+++ src/Components/Forms.jsx
@@ -5,17 +5,12 @@ import ResultContainer from "./ResultContainer";
 
 const Forms = props => {
 
-    const form1 = props.state.form1IsVisible ? <Entry1Container/> : '';
-    const form2 = props.state.form2IsVisible ? <Entry2Container/> : '';
-    const form3 = props.state.registrationIsVisible ? <RegistrationContainer/> : '';
-    const results = props.state.resultIsVisible ? <ResultContainer/> : '';
-
     return (
         <div>
-            {form1}
-            {form2}
-            {form3}
-            {results}
+            {props.state.form1IsVisible && <Entry1Container/>}
+            {props.state.form2IsVisible && <Entry2Container/> }
+            {props.state.registrationIsVisible && <RegistrationContainer/>}
+            {props.state.resultIsVisible && <ResultContainer/>}
         </div>
     )
 }
